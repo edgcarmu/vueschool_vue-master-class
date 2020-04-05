785927a852654cd73642671e8bbc174cb2eb9e8e
Fabian Carvajal Murillo <inbox@edgcarmu.me>
2020-04-05 16:45:05 -0600

    render threads and posts

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Welcome to the forum</h1>
+    <div v-for="thread in threads">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data.json'
+
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        threads: sourceData.threads,
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
