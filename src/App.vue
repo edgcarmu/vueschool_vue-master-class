1e728cf23c6f7d0d06327957ff2b316c17da0288
Fabian Carvajal Murillo <inbox@edgcarmu.me>
2020-04-25 18:21:25 -0600

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <TheNavBar/>
     <div class="container">
       <router-view/>
     </div>
@@ -7,20 +8,16 @@
 </template>
 
 <script>
+  import TheNavBar from './components/TheNavBar'
+
   export default {
-    name: 'app'
+    name: 'app',
+    components: {
+      TheNavBar
+    }
   }
 </script>
 
 <style>
   @import "assets/css/style.css";
-
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
